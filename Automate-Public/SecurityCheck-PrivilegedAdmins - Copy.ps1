328c744e91b81d58b291533ec104cef367ad7a38
digacore <nberger@digacore.com>
2023-02-02 09:56:57 -0500

    Push

---
+++ Automate-Public/SecurityCheck-PrivilegedAdmins - Copy.ps1
@@ -0,0 +1,26 @@
+Function Get-PrivilegedGroupChanges {            
+    Param(            
+        $Server = (Get-ADDomainController -Discover | Select-Object -ExpandProperty HostName),            
+        $Hour = 24            
+    )            
+                
+        $ProtectedGroups = Get-ADGroup -Filter 'AdminCount -eq 1' -Server $Server            
+        $Members = @()            
+                
+        ForEach ($Group in $ProtectedGroups) {            
+            $Members += Get-ADReplicationAttributeMetadata -Server $Server `
+                -Object $Group.DistinguishedName -ShowAllLinkedValues |            
+             Where-Object {$_.IsLinkValue} |            
+             Select-Object @{name='GroupDN';expression={$Group.DistinguishedName}}, `
+                @{name='GroupName';expression={$Group.Name}}, *            
+        }            
+                
+        $Members |            
+            Where-Object {$_.LastOriginatingChangeTime -gt (Get-Date).AddHours(-1 * $Hour)}            
+                
+    }            
+                
+                
+    # Last 24 hours            
+    $results = Get-PrivilegedGroupChanges | format-list -Property Groupname,AttributeValue,LastOriginatingChangeTime,Server
+    Write-Host $results
